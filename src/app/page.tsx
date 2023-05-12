9384520872ef474d82a373ca309f257d3f84a280
kenvilar <kenvilar@gmail.com>
2023-05-12 18:13:51 +0800

    implement feature tabs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "@/app/components/Header";
 import Hero from "@/app/components/Hero";
 import Features from "@/app/components/Features";
+import FeatureTabs from "@/app/components/FeatureTabs";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <Hero />
 
       <Features />
+
+      <FeatureTabs />
     </>
   );
 }
