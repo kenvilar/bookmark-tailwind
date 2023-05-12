559686a8f7d86d2dc9bbf673c75b3ff218a94aa8
kenvilar <kenvilar@gmail.com>
2023-05-12 17:52:09 +0800

    implement hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+
+      <Hero />
     </>
   );
 }
