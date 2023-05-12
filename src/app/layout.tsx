082a437062de5a83f92f8b3888d46b8bb404658b
kenvilar <kenvilar@gmail.com>
2023-05-12 17:26:48 +0800

    config rubik font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ weight: ["400", "600", "700"], subsets: ["latin"] });
 
 export const metadata = {
   title: "Bookmark - Ken Vilar",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={rubik.className}>{children}</body>
     </html>
   );
 }
