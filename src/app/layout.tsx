20a4d21057c05df197f742b8c8cc8b22544c6b59
kenvilar <kenvilar@gmail.com>
2023-05-12 17:31:04 +0800

    add images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={rubik.className}>{children}</body>
+      <body className={`${rubik.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
