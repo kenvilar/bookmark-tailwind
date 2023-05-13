532433a31c6ed5a2c505710c80a7ab88af6b741a
kenvilar <kenvilar@gmail.com>
2023-05-13 21:44:36 +0800

    implement download section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/app/components/Header";
 import Hero from "@/app/components/Hero";
 import Features from "@/app/components/Features";
 import FeatureTabs from "@/app/components/FeatureTabs";
+import Download from "@/app/components/Download";
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <Features />
 
       <FeatureTabs />
+
+      <Download />
     </>
   );
 }
