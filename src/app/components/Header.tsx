c5f2c4a6ffb89b65743a880c2db57b1777febd14
kenvilar <kenvilar@gmail.com>
2023-05-20 19:04:20 +0800

    add mobile menu items

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -44,6 +44,33 @@ const Header = () => {
             <span className="hamburger-bottom" />
           </button>
         </div>
+
+        {/*mobile menu*/}
+        <div
+          id="menu"
+          className="fixed inset-0 z-20 flex flex-col items-center self-end w-full h-full min-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue"
+        >
+          <div className="w-full py-3 text-center">
+            <a href="#features" className="block hover:text-softRed">
+              Features
+            </a>
+          </div>
+          <div className="w-full py-3 text-center">
+            <a href="#download" className="block hover:text-softRed">
+              Download
+            </a>
+          </div>
+          <div className="w-full py-3 text-center">
+            <a href="#faq" className="block hover:text-softRed">
+              FAQ
+            </a>
+          </div>
+          <div className="w-full py-3 text-center">
+            <a href="#" className="block hover:text-softRed">
+              Login
+            </a>
+          </div>
+        </div>
       </nav>
     </>
   );
