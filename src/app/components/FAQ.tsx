44a1b3935bd839859782f5d8280ffaf2583b4ab0
kenvilar <kenvilar@gmail.com>
2023-05-20 19:15:33 +0800

    minor change

--- src/app/components/FAQ.tsx
+++ src/app/components/FAQ.tsx
@@ -7,8 +7,8 @@ const FAQ = () => {
             Frequently Asked Questions
           </h2>
           <p className="max-w-lg px-6 mx-auto text-center text-grayishBlue">
-            Here are some of our FAQs. If you have any other questions you'd
-            like answered please feel free to email us.
+            {`Here are some of our FAQs. If you have any other questions you'd
+            like answered please feel free to email us.`}
           </p>
         </div>
       </section>
